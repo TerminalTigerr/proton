970be136514f058c54976ca42f0aa7434c5c0d65
BitHero <nuhumusah874@gmail.com>
2023-07-18 19:36:35 +0000

    responsivenes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 /*========== Mobile Menu==========*/
 const navBtn = document.getElementById('menu-btn')
 const navMenu = document.getElementById('menu')
+const navLink = document.querySelectorAll('nav-link')
 
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('open')
     navMenu.classList.toggle('flex')
     navMenu.classList.toggle('hidden')
+})
+
+/*========== NavLink ==========*/
+const linkAction = () => {
+    navMenu.classList.toggle('hidden')
+}
+navLink.forEach(link, () => {
+    link.addEventListener('click', linkAction)
 })
